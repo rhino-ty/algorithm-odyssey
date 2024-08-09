af64793cef56abcb6a11a56747d433d4548a4100
Rhino-ty <okko8522@gmail.com>
2024-08-09 20:59:30 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  return s.toLowerCase().split("p").length === s.toLowerCase().split("y").length;
+}
+
+// 2022-10-17 18:14:51
+function solution(s) {
+  let count = 0;
+  let sLower = s.toLowerCase();
+  for (i = 0; i < sLower.length; i++) {
+    if (sLower[i] === "p") {
+      count += 1;
+    }
+    if (sLower[i] === "y") {
+      count -= 1;
+    }
+  }
+  return count === 0 ? true : false;
+}
