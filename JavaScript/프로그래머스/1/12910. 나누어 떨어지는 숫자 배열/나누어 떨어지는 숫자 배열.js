cc72f14f595f33ec26bff7bad15705cca11ec706
Rhino-ty <okko8522@gmail.com>
2024-08-09 20:54:59 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 27.98 ms, Memory: 38.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++ JavaScript/프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -1,15 +1,23 @@
+// filter로 해결할 수 있을 거 같아 추가 작성함
 function solution(arr, divisor) {
-    let answer = [];
-    if (arr.length > 0) {
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor === 0) {
-                answer.push(arr[i])
-            }
-        }
-    }
-    if (answer.length === 0) {
-        answer.push(-1)
-    }
-    answer.sort((a, b) => a - b)
-    return answer;
+    arr.sort((a, b) => a - b)
+    const arrFilter = arr.filter(num => num % divisor === 0)
+    return arrFilter.length ? arrFilter : [-1]
 }
+
+// 2022-09-07 19:09:03
+// function solution(arr, divisor) {
+//     let answer = [];
+//     if (arr.length > 0) {
+//         for (i = 0; i < arr.length; i++) {
+//             if (arr[i] % divisor === 0) {
+//                 answer.push(arr[i])
+//             }
+//         }
+//     }
+//     if (answer.length === 0) {
+//         answer.push(-1)
+//     }
+//     answer.sort((a, b) => a - b)
+//     return answer;
+// }
