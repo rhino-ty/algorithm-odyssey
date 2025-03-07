1d611ee4c0e56c34c2f54a7814a33cd44cd0b116
Taeyeon Yoon <okko8522@gmail.com>
2025-03-07 10:03:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 204 ms, Memory: 10616 KB -BaekjoonHub

--- node.js/백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ node.js/백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,23 +1,23 @@
-function getMaxThingsInBag(N, K, things) {
-  const dp = Array(N + 1)
-    .fill()
-    .map(() => Array(K + 1).fill(0));
+// function getMaxThingsInBag(N, K, things) {
+//   const dp = Array(N + 1)
+//     .fill()
+//     .map(() => Array(K + 1).fill(0));
 
-  // DP 배열 채우기
-  for (let i = 1; i <= N; i++) {
-    const [weight, value] = things[i - 1]; // 0-based 배열을 1-based로 변환
+//   // DP 배열 채우기
+//   for (let i = 1; i <= N; i++) {
+//     const [weight, value] = things[i - 1]; // 0-based 배열을 1-based로 변환
 
-    for (let w = 0; w <= K; w++) {
-      if (weight > w) {
-        dp[i][w] = dp[i - 1][w];
-      } else {
-        dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weight] + value);
-      }
-    }
-  }
+//     for (let w = 0; w <= K; w++) {
+//       if (weight > w) {
+//         dp[i][w] = dp[i - 1][w];
+//       } else {
+//         dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weight] + value);
+//       }
+//     }
+//   }
 
-  return dp[N][K];
-}
+//   return dp[N][K];
+// }
 
 // 1. 각 무게에서의 최대 가치를 배열로 관리하는 DP 방식 채택
 // 2. 무게 한도(K)부터 현재 물건의 무게까지 역순으로 순회
@@ -25,25 +25,25 @@ function getMaxThingsInBag(N, K, things) {
 //   - 만약 순방향(0부터 K까지)으로 순회하면, 현재 물건을 추가한 후 갱신된 dp[w] 값을 사용해 같은 물건을 또 한 번 추가될 수 있음
 //   - 역순으로 순회하면, 현재 물건을 고려하기 전의 상태(dp[w - weight])를 사용하게 되어, 한 물건을 중복해서 사용하는 것을 방지
 
-// function getMaxThingsInBag(N, K, things) {
-//   const dp = Array(K + 1).fill(0);
+function getMaxThingsInBag(N, K, things) {
+  const dp = Array(K + 1).fill(0);
 
-//   // 각 물건에 대해 처리
-//   for (let i = 0; i < N; i++) {
-//     const [weight, value] = things[i];
+  // 각 물건에 대해 처리
+  for (let i = 0; i < N; i++) {
+    const [weight, value] = things[i];
 
-//     // for (let w = weight; w <= K; w++) {
-//     //   dp[w] = Math.max(dp[w], dp[w - weight] + value);
-//     //   console.log(...dp);
-//     // }
+    // for (let w = weight; w <= K; w++) {
+    //   dp[w] = Math.max(dp[w], dp[w - weight] + value);
+    //   console.log(...dp);
+    // }
 
-//     for (let w = K; w >= weight; w--) {
-//       dp[w] = Math.max(dp[w], dp[w - weight] + value);
-//     }
-//   }
+    for (let w = K; w >= weight; w--) {
+      dp[w] = Math.max(dp[w], dp[w - weight] + value);
+    }
+  }
 
-//   return dp[K];
-// }
+  return dp[K];
+}
 
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf-8').split('\n');
